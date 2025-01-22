f4e07139304c296abe64b4a970dddefd73cf6022
AsthuX <osm0348@naver.com>
2025-01-22 15:16:59 +0900

    feat: openfeign interface 구현 및 예전 commit 복구작업

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,29 +3,36 @@ plugins {
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
 }
+val springAiVersion by extra("1.0.0-M4")
+val springCloudVersion by extra("2024.0.0")
 
 group = "com.ll"
 version = "0.0.1-SNAPSHOT"
-
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
 	}
 }
-
 configurations {
 	compileOnly {
 		extendsFrom(configurations.annotationProcessor.get())
 	}
 }
-
 repositories {
 	mavenCentral()
+	maven {
+		url = uri("https://repo.spring.io/milestone")
+	}
 }
 
+
 dependencies {
+	//openfeign
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
@@ -34,11 +41,15 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
-
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
+		mavenBom("org.springframework.ai:spring-ai-bom:$springAiVersion")
+	}
+}
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
-
 tasks.jar {
 	enabled = false
 }
