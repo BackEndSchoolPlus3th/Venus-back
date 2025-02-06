bb19be455641c7aa9dc1e383d40f00677261e651
LSH-kw0315 <tmdgjsdl1234@naver.com>
2025-02-06 09:46:46 +0900

    remove dependency

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -50,11 +50,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
 
-	// jwt
-	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5") // JSON 직렬화 필요
-
 	//swagger
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
